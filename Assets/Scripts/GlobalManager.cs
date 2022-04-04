d8328f92bb57c08217e977929aecde957e846a23
Enjl <enjlectric@gmail.com>
2022-04-04 22:46:54 +0200

    pause menu

--- Assets/Scripts/GlobalManager.cs
+++ Assets/Scripts/GlobalManager.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GlobalManager : MonoBehaviour
 {
     public enum GameMode { Singleplayer, Multiplayer }
 
     public GameMode gameMode;
+
+    public CanvasGroup blackCanvas;
 
     private static GlobalManager _instance;
 
@@ -18,6 +21,22 @@ public class GlobalManager : MonoBehaviour
             if (_instance == null)
             {
                 _instance = new GameObject().AddComponent<GlobalManager>();
+                Canvas c = _instance.gameObject.AddComponent<Canvas>();
+                c.sortingOrder = 999;
+                c.sortingLayerName = "UIForeground";
+                c.renderMode = RenderMode.ScreenSpaceOverlay;
+                RectTransform child = new GameObject().AddComponent<RectTransform>();
+                Image i = child.gameObject.AddComponent<Image>();
+                i.color = Color.black;
+                child.SetParent(_instance.transform);
+                _instance.blackCanvas = child.gameObject.AddComponent<CanvasGroup>();
+                _instance.blackCanvas.alpha = 0;
+                _instance.blackCanvas.blocksRaycasts = false;
+                _instance.blackCanvas.interactable = false;
+                child.anchorMin = Vector2.zero;
+                child.anchorMax = Vector2.one;
+                child.anchoredPosition = Vector2.zero;
+                child.sizeDelta = Vector2.one;
             }
 
             return _instance;
@@ -43,7 +62,7 @@ public class GlobalManager : MonoBehaviour
 
     public void LoadMainMenu()
     {
-        SceneManager.LoadScene(0);
+        StartCoroutine(LoadMain());
     }
 
     public void StartSingleplayer()
@@ -65,14 +84,36 @@ public class GlobalManager : MonoBehaviour
 
     private IEnumerator LoadMain()
     {
-        SceneManager.LoadScene(0);
-        yield return null;
+        yield return FadeOut(0);
     }
 
     private IEnumerator LoadGame()
     {
-        SFX.UIStartGame.Play();
-        yield return new WaitForSeconds(4);
-        SceneManager.LoadScene(1);
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            SFX.UIStartGame.Play();
+        }
+        yield return FadeOut(1);
+    }
+
+    private IEnumerator FadeOut(int idxToLoad)
+    {
+        AudioManager.ChangeMusic(null);
+        blackCanvas.interactable = true;
+        blackCanvas.blocksRaycasts = true;
+        while (blackCanvas.alpha < 1)
+        {
+            blackCanvas.alpha += Time.deltaTime;
+            yield return null;
+        }
+        SceneManager.LoadScene(idxToLoad);
+
+        blackCanvas.interactable = false;
+        blackCanvas.blocksRaycasts = false;
+        while (blackCanvas.alpha > 0)
+        {
+            blackCanvas.alpha -= Time.deltaTime;
+            yield return null;
+        }
     }
 }
---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    public CanvasGroup ownGroup;
+    public AudioMixer masterMixer;
+
+    public Slider musicVolSlider;
+    public Slider soundVolSlider;
+
+    private void OnEnable()
+    {
+        masterMixer.GetFloat("musicVol", out float m);
+        masterMixer.GetFloat("soundVol", out float s);
+        musicVolSlider.SetValueWithoutNotify(m);
+        soundVolSlider.SetValueWithoutNotify(s);
+    }
+
+    public void Pause()
+    {
+        if (ownGroup.alpha == 1)
+        {
+            Resume();
+            return;
+        }
+
+        if (BattleManager.state == BattleManager.BattleState.Cutscene || BattleManager.state == BattleManager.BattleState.Battle)
+        {
+            EnablePause();
+        }
+    }
+
+    public void EnablePause()
+    {
+        ownGroup.blocksRaycasts = true;
+        ownGroup.alpha = 1;
+        Time.timeScale = 0;
+        ownGroup.interactable = true;
+    }
+
+    public void DisablePause()
+    {
+        ownGroup.blocksRaycasts = false;
+        ownGroup.alpha = 0;
+        Time.timeScale = 1;
+        ownGroup.interactable = false;
+    }
+
+    public void Resume()
+    {
+        DisablePause();
+    }
+    public void Retry()
+    {
+        DisablePause();
+        GlobalManager.instance.StartSingleplayer();
+    }
+
+    public void SetMusicGroupVolume(float volume)
+    {
+        masterMixer.SetFloat("musicVol", Mathf.Log(volume) * 20);
+    }
+
+    public void SetSFXGroupVolume(float volume)
+    {
+        masterMixer.SetFloat("soundVol", Mathf.Log(volume) * 20);
+    }
+
+    public void GiveUp()
+    {
+        DisablePause();
+        GlobalManager.instance.LoadMainMenu();
+    }
+}
